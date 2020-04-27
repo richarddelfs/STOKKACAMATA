eb5655a8d1812b6fc205a2c569119f1fc1e775e9
Filbert <filberthtwn@gmail.com>
2020-04-28 04:06:55 +0700

    Update Project

--- app/src/main/java/com/example/stokkacamata/EmployeeInterface.java
+++
@@ -1,7 +0,0 @@
-package com.example.stokkacamata;
-
-import java.io.Serializable;
-
-public interface EmployeeInterface extends Serializable {
-    void onItemClick(ProfilePegawai profilePegawai);
-}
